6b99a2aa6b98c8eb0fc3751adfd6eb29b4290095
nadim <achmadnadim@gmail.com>
2025-02-27 22:03:49 +0700

    Create SearchBar composable and integrate it into MainActivity
    - Created a new `SearchBar` composable function in `SearchBar.kt` to implement a material search bar.
    - Implemented the SearchBar in `MainActivity.kt` within the `MainContent` composable.
    - Added search text, microphone icon, and `TextField` with custom styling.
    - Added a background to the `Scaffold` for `MainActivity.kt`.
    - Added scroll state to `MainActivity.kt`.

--- app/src/main/java/com/arbeat/movieapps/activity/MainActivity.kt
+++ app/src/main/java/com/arbeat/movieapps/activity/MainActivity.kt
@@ -6,25 +6,34 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.arbeat.movieapps.BottomNavigationBar
 import com.arbeat.movieapps.R
+import com.arbeat.movieapps.SearchBar
 import com.arbeat.movieapps.domain.FilmItemModel
 
 class MainActivity : ComponentActivity() {
@@ -76,12 +85,35 @@ fun MainScreen(onItemClick: (FilmItemModel) -> Unit = {})
             modifier = Modifier
                 .padding(paddingValues)
                 .background(color = colorResource(id = R.color.blackBackground))
+        ){
+            Image(
+                painter = painterResource(id=R.drawable.bg1),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+
+        MainContent(onItemClick)
+    }
+}
+
+@Composable
+fun MainContent(onItemClick: (FilmItemModel) -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(top = 60.dp, bottom = 100.dp)
+    ) {
+        Text(
+            text = "What would you like to watch?",
+            style = TextStyle(color = Color.White, fontSize = 25.sp),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, bottom = 16.dp)
+                .fillMaxWidth()
         )
-        Image(
-            painter = painterResource(id=R.drawable.bg1),
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize()
-        )
+        SearchBar(hint = "Search Movies...")
     }
 }
